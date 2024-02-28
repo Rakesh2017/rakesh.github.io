c9a4915e0dbecf6c6caa90cbecb53da56faa1bc3
rakesh <rakesh@rakeshs-MacBook-Pro-2.local>
2024-02-27 19:28:37 -0800

    improve portfolio

--- src/components/work-experience.tsx
+++ src/components/work-experience.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ExpTemplate from "./exp-template";
 
 const WorkExperience = () => {
-  const inter_dev = "Intermediate Android Developer";
+  const inter_dev = "Intermediate Mobile Developer";
   const dev = "Android Developer";
   const freelancer = "Freelancer Android Developer";
   return (
@@ -10,32 +10,36 @@ const WorkExperience = () => {
       <div>
         <h4 className="work-exp-h4">Work Experience</h4>
       </div>
-      <div className='exp-content-con'>
-      <ExpTemplate
-          company={"Iversoft Solutions"}
-          designation={inter_dev}
-          date={"May 2022 - Feb 2024"}
-        />
-      <ExpTemplate
-          company={"Skythinking Technology Co. Ltd."}
-          designation={dev}
-          date={"May 2021 - April 2022"}
-        />
-        <ExpTemplate
-          company={"IntelligentOutsourcing"}
-          designation={dev}
-          date={"December 2018 - April 2019"}
-        />
-        <ExpTemplate
-          company={"CarByke"}
-          designation={freelancer}
-          date={"June 2018 - November 2018"}
-        />
-        <ExpTemplate
-          company={"Enhabyto IT Solutions"}
-          designation={dev}
-          date={"August 2017 - May 2018"}
-        />
+      <div style={{ position:"relative" }}>
+        <div id="line1" className="horizontal-line" />
+        <div id="line2" className="horizontal-line" />
+        <div className='exp-content-con'>
+          <ExpTemplate
+            company={"Iversoft Solutions"}
+            designation={inter_dev}
+            date={"May 2022 - Feb 2024"}
+          />
+          <ExpTemplate
+            company={"Skythinking Technology Co. Ltd."}
+            designation={dev}
+            date={"May 2021 - April 2022"}
+          />
+          <ExpTemplate
+            company={"IntelligentOutsourcing"}
+            designation={dev}
+            date={"December 2018 - April 2019"}
+          />
+          <ExpTemplate
+            company={"CarByke"}
+            designation={freelancer}
+            date={"June 2018 - November 2018"}
+          />
+          <ExpTemplate
+            company={"Enhabyto IT Solutions"}
+            designation={dev}
+            date={"August 2017 - May 2018"}
+          />
+        </div>
       </div>
     </div>
   );
